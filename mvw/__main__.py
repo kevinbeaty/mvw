6ad248c192fc6ea32bec32c0d0c8365214544032
Kevin Beaty <kevin@simplectic.com>
2011-12-09 23:21:38 -0600

    Parse command line options in __main__

--- mvw/__main__.py
+++ mvw/__main__.py
@@ -1,3 +1,22 @@
+from generator import Generator
+from optparse import OptionParser
+
 if __name__ == '__main__':
-    from generator import Generator
-    Generator().run()
+
+    usage = """%prog [options] [SOURCEDIR]
+            If SOURCEDIR not given, wiki is assumed"""
+    desc = """Minimal Viable Wiki
+            http://mvw.simplectic.com"""
+    version = "0.0.1"
+
+    opts = OptionParser(usage=usage, description=desc, version=version)
+    opts.add_option('-d', '--destination', dest='destination', default='_site',
+                    help='Source directory of the wiki files')
+    (options, args) = opts.parse_args()
+
+    if len(args) == 0:
+        source = 'wiki'
+    else:
+        source = args[0]
+
+    Generator().run(source, options.destination)
