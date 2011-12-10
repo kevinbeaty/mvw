3ec54fbabd6f17eabb90ead66a87bc2723a00aa0
Kevin Beaty <kevin@simplectic.com>
2011-12-10 17:56:51 -0600

    Create destination path if it does no exist and copy file using shutil

--- mvw/generator.py
+++ mvw/generator.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 class Generator:
 
@@ -8,6 +9,8 @@ class Generator:
         prefix = len(sourcedir)+len(os.path.sep)
         for root, dirs, files in os.walk(sourcedir):
             destpath = os.path.join(outputdir, root[prefix:])
+            if not os.path.exists(destpath):
+                os.makedirs(destpath)
 
             print()
             print('-'*25)
@@ -21,7 +24,7 @@ class Generator:
                     self.parse(src, dest)
                 else:
                     dest = os.path.join(destpath, f)
-                    self.copy(src, dest)
+                    shutil.copy(src, dest)
 
             print('-'*25)
             print('Dirs')
@@ -31,5 +34,3 @@ class Generator:
     def parse(self, source, destination): 
         print("Parse Source: %s Destination: %s" % (source, destination))
 
-    def copy(self, source, destination):
-        print("Copy Source: %s Destination: %s" % (source, destination))
