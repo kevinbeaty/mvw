19cf9e5dfee3234b190965867c4dc22eb2b99eb4
Kevin Beaty <kevin@simplectic.com>
2011-12-11 20:46:34 -0600

    Add setup.py and MANIFEST

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""
+MVw
+---
+Minimal Viable Wiki
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='MVW',
+    version='0.1pre',
+    url='http://github.com/simplectic/mvw/',
+    license='MIT',
+    author='Kevin Beaty',
+    author_email='kevin@simplectic.com',
+    description='A minimal personal wiki engine',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'mako',
+        'markdown',
+        'pygments',
+    ],
+    entry_points={
+      'console_scripts': [
+          'mvw = mvw.__main__:run'
+      ]
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+    ],
+)
